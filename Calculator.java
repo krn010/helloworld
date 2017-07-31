1fbfe5a5de091de40ee99dd8c10c96793c92ab8e
krn010 <metalheadkrn@gmail.com>
2017-07-31 17:37:58 +0530

    finals

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Calculator 
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+double fnum,snum,ans;
+System.out.println("Enter the first number : ");
+fnum = sc.nextDouble();
+System.out.println("Enter the second number : ");
+snum = sc.nextDouble();
+System.out.println(ans);
+ans = fnum+snum;
+}
+}
